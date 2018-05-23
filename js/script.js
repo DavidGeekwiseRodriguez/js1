4229ec1eca6a8bdc213cbfcd1af9fb28287993bc
David <rodriguezdavid99@gmail.com>
2018-05-23 12:11:32 -0700

    takehome branch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-var myName = "David";
+var myName = "David Rodriguez";
+var favHobby = "favorite hobby is video editing.";
+var classGoals = "to fully understand javascript.";
 
-console.log(myName);
+console.log("My name is " + myName + " and my " + favHobby + " My goals are to " + classGoals);
