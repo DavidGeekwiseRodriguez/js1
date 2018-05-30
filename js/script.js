0a597b0579ae5ff795e748f5b29fb652ab7ad891
David <rodriguezdavid99@gmail.com>
2018-05-29 17:30:27 -0700

    finished the try it out and the take home

--- js/script.js
+++ js/script.js
@@ -77,3 +77,65 @@ let myNumber = 77;
 //     alert('lol u dont have a name');
 //     break;
 // }
+
+//TRY IT OUT
+
+// let askAge = parseInt(prompt('how old are u'));
+
+// if (askAge >= 18) {
+//   alert('you may pass');
+//   var yesOrNo = prompt('are you younger than 80');
+//   if (yesOrNo == 'yes') {
+//     let starWars = prompt('do you like star wars');
+//     if (starWars == 'yes') {
+//       alert('u are beautiful and welcome');
+//     } else {
+//       alert('go back to star trek weirdo');
+//     }
+//   } else if (yesOrNo == 'no') {
+//     alert('i bet u like prunes u old fart');
+//   }
+// } else {
+//   alert('you shall not pass');
+// }
+
+//TAKE HOME
+
+let askName = prompt('you whats ur name');
+let askFavColor = prompt('whats ur fav color', 'red, orange, yellow, green, blue, pink, purple, black, white, gray');
+
+switch(askFavColor) {
+  case 'red':
+    alert(`eww ${askName} likes red`);
+    break;
+  case 'orange':
+    alert(`${askName}, it aint the new black`);
+    break;
+  case 'yellow':
+    alert(`${askName}'s shining like he's pikachu`);
+    break;
+  case 'green':
+    alert(`ah so ${askName}'s green like shrek`);
+    break;
+  case 'blue':
+    alert(`dont be down ${askName}, be happy`);
+    break;
+  case 'pink':
+    alert(`gotta agree with ${askName}, it's pretty sick`);
+    break;
+  case 'purple':
+    alert(`${askName}'s lookin like grimace`);
+    break;
+  case 'black':
+    alert(`ow don't cut yourself on ${askName}'s edge`);
+    break;
+  case 'white':
+    alert(`${askName} might be an angel`);
+    break;
+  case 'gray':
+    alert(`${askName}, it's just a dark white`);
+    break;
+  default:
+    alert(`don't make me say bad things about you, ${askName}`);
+    break;
+}
