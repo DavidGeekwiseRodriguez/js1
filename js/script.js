1f2d70b77f018f56779f49c015233e252a5a544e
David <rodriguezdavid99@gmail.com>
2018-06-04 18:04:42 -0700

    day4 notes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,113 @@
-//TAKE HOME
+let inside = true;
 
-let askName = prompt('you whats ur name');
-let askFavColor = prompt('whats ur fav color', 'red, orange, yellow, green, blue, pink, purple, black, white, gray');
-
-switch(askFavColor) {
-  case 'red':
-    alert(`eww ${askName} likes red`);
-    break;
-  case 'orange':
-    alert(`${askName}, it aint the new black`);
-    break;
-  case 'yellow':
-    alert(`${askName}'s shining like he's pikachu`);
-    break;
-  case 'green':
-    alert(`ah so ${askName}'s green like shrek`);
-    break;
-  case 'blue':
-    alert(`dont be down ${askName}, be happy`);
-    break;
-  case 'pink':
-    alert(`gotta agree with ${askName}, it's pretty sick`);
-    break;
-  case 'purple':
-    alert(`${askName}'s lookin like grimace`);
-    break;
-  case 'black':
-    alert(`ow don't cut yourself on ${askName}'s edge`);
-    break;
-  case 'white':
-    alert(`${askName} might be an angel`);
-    break;
-  case 'gray':
-    alert(`${askName}, it's just a dark white`);
-    break;
-  default:
-    alert(`don't make me say bad things about you, ${askName}`);
-    break;
+if (inside) {
+  // console.log('i am inside');
+} else {
+  // console.log('i am not inside');
 }
+
+// condition ? expression1 : expression2
+
+let example2 = inside ? 'I am inside' : 'i am not inside';
+// console.log(example2);
+
+// let number = 10;
+
+// let num = (number > 20) ? 'number is greater than 20' : 'number is less than 20';
+// let num - can assign function to a variable
+// (number > 20) ? - condition - if number is greater than 20
+// 'number is greater than 20' : - if the condition is true
+// 'number is less than 20'; - if the condition is false
+
+// console.log(num);
+
+// let checkEmail = name ? 'i have name' : email ? 'i have email' : 'i have none';
+
+// console.log(checkEmail);
+
+let iAmHungry = true;
+let money = 5;
+
+if (iAmHungry === true) {
+  if (money > 10) {
+    // console.log('i am hungry and i have more than 10');
+  } else {
+    // console.log('i am hungry and i have less than 10');
+  }
+} else {
+  if (money > 10) {
+    // console.log('i am not hungry and i have more than 10');
+  } else {
+    // console.log('i am not hungry and have less than 10');
+  }
+}
+// console.log(iAmHungry);
+
+let hungryQuestion = iAmHungry ? ( (money > 10) ? 'i am hungry and have more than 10' : 'i am hungry and have less than 10') :
+            ( (money > 10) ? 'i am not hungry and i have more than 10' : 'i am not hungry and have less than 10');
+// console.log(iAmHungry);
+
+let iAmWearingYellow = false;
+
+let yellowQuestion = iAmWearingYellow ? 'yes, i am wearing yellow' : 'no, im not wearing yellow';
+// if iAmWearingYellow is false, it runs the second string
+// if iAmWearingYellow was true, it would've ran the first string
+// console.log(yellowQuestion);
+
+let number = 23;
+
+let num = (number && iAmWearingYellow) ? 'yes, i am 23 and wearing yellow' : 'i am not 23 and i dont wear yellow';
+// console.log(num);
+
+let arr = ['javascript', 100, {}, [], true];
+// console.log(arr[0]);
+
+var arrQnA = ['Baby Driver', 99, true, 2017];
+// console.log(arrQnA);
+
+// arrQnA.push('sushi');
+// .push() will add to the end of the array
+// console.log(arrQnA);
+// arrQnA.unshift('sushi');
+// .unshift() will add to the beginning of the arraya
+// console.log(arrQnA);
+// arrQnA.pop();
+// .pop() will remove the last item in the array
+// console.log(arrQnA);
+// .shift() will remove the first item in the arraya
+
+// console.log(arrQnA[arrQnA.length - 1]);
+// finding how many items are in the array
+
+// arrQnA.splice(2);
+// .splice() will get the item index and remove items past that
+// console.log(arrQnA);
+
+// arrQnA.splice(2, 0, 'cat');
+// .splice() can also insert and replace at the index
+// console.log(arrQnA);
+
+// arrQnA.slice();
+// .slice() will make a copy of the array
+// console.log(arrQnA);
+
+const products = [['blanket', 12.99, 'the best blanket around'], ['rattle', 4.99, 'it rattles man'], ['diapers', 12.99, 'they catch the doo doo']];
+// console.log(products);
+// console.log(products[0][1]);
+
+let firstArray = ['Baby Driver', 'Baby'];
+let secArray = ['Pokemon: The First Movie', 'Ash'];
+let thirdArray = ['The Martian', 'Wattney'];
+let fourthArray = ['Guardians of the Galaxy', 'Peter'];
+let fifthArray = ['Pulp Fiction', 'Vincent'];
+
+var favMovies = [];
+favMovies.push(firstArray, secArray, thirdArray, fourthArray, fifthArray);
+
+var movieTitles = [];
+movieTitles.push(favMovies[0][0], favMovies[1][0], favMovies[2][0], favMovies[3][0], favMovies[4][0], favMovies[5][0]);
+
+var favAndLeastFav = [];
+favAndLeastFav.push(favMovies[0][0], favMovies[3][0]);
+
+alert(favMovies.length);
