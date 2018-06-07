6825ed7400c5e0a82d7f4c0822144800c619e0e0
David <rodriguezdavid99@gmail.com>
2018-06-06 19:51:58 -0700

    adding to the day5 notes

--- js/script.js
+++ js/script.js
@@ -14,18 +14,18 @@
 
 // FUNCTIONS
 
-function capitalCase(string) {
-  let words = string.split(' ');
-  words.forEach(word => {
-    let letters = word.split('');
-    let capped = letters[0].toUpperCase();
-    letters.splice(0, 1, capped);
-  });
-  return words.join(' ');
-};
+// function capitalCase(string) {
+//   let words = string.split(' ');
+//   words.forEach(word => {
+//     let letters = word.split('');
+//     let capped = letters[0].toUpperCase();
+//     letters.splice(0, 1, capped);
+//   });
+//   return words.join(' ');
+// };
 // the function argument does not have a value until it is called
 
-let newString = capitalCase('peter piper');
+// let newString = capitalCase('peter piper');
 // string now has a value of peter piper
 // console.log(newString);
 
@@ -39,8 +39,41 @@ let newString = capitalCase('peter piper');
 
 // alert(dogAge(parseInt(prompt('how old is ur dog', 'in human years'))));
 
-function personSupply(age, product) {
-  
+// step 1
+let todos = [];
+
+let todo1 = [prompt("What do you want to do first?", "Eat")];
+let todo2 = [prompt("What do you want to do second?", "Sleep")];
+let todo3 = [prompt("What do you want to do third?", "Eat")];
+
+todos.push(todo1, todo2, todo3);
+
+// step 2
+// i = 0 - the starting value, i < todos.length - where it ends, i++ - incrementer
+for(let i = 0; i < todos.length; i++) {
+  let timeline = prompt(`How many days will it take to get ${todos[i][0]} done?`, "2");
+  timeline = !isNaN(parseInt(timeline)) ? parseInt(timeline) : 2;
+  todos[i].unshift(timeline);
 }
 
-alert(personSupply(parseInt(prompt('how old are you fam'))));
+todos.sort();
+
+// step 3
+let longest = todos[0];
+let j = 0;
+while(j < todos.length) {
+  if(todos[j][0] > longest[0]) longest = todos[j];
+  // longest = todos[j][0] > longest[0] ? longest = todos[j] : longest = longest; // ternary version
+  j++;
+}
+
+longest.push("these are not the droids you're looking for");
+
+// step 4
+let z = 0;
+do {
+  if(todos[z][todos[z].length - 1] !== "this") {
+    todos[z].push("done");
+  }
+  z++;
+} while(z < todos.length);
